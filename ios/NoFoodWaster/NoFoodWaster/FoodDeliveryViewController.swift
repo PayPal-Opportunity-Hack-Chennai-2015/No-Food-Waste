6bad1986bcc707f81a04124266f48e86aa0be42a
RShankar <ravi@rshankar.com>
2015-11-29 06:44:34 +0530

    API Call for donate and available donations

--- ios/NoFoodWaster/NoFoodWaster/FoodDeliveryViewController.swift
+++ ios/NoFoodWaster/NoFoodWaster/FoodDeliveryViewController.swift
@@ -13,14 +13,23 @@ class FoodDeliveryViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "Food Delivery Places"
+        
+        applyStyle()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func applyStyle() {
+        descriptionLabel.backgroundColor = backgroundColor
+        view.backgroundColor = backgroundColor
     }
 }
